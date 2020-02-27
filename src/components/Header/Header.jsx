e5d4a695911860d7e5bccd02561559bf3ed39ee8
rasolko <vano2029492@mail.ru>
2020-02-27 15:00:11 +0300

    login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
             <img src='https://cdn.motor1.com/images/mgl/GwZbJ/s3/logo-story-volkswagen.jpg'></img>
+            <div className={s.login}>
+                {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+            </div>
         </header>
     )
 }
